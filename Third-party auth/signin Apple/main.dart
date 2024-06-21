7019f1c4e9069378754c7b6f2cb2c14acd11c4e9
Noble Okafor <okafornoble202@gmail.com>
2024-06-21 17:41:00 +0100

    apple sign in

---
+++ Third-party auth/signin Apple/main.dart
@@ -0,0 +1,289 @@
+ import 'package:flutter/material.dart';
+ import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
+ import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+ void main() async {
+   WidgetsFlutterBinding.ensureInitialized()
+   final keyApplicationId = 'YOUR_APP_ID_HERE';
+   final keyClientKey = 'YOUR_CLIENT_KEY_HERE';
+   final keyParseServerUrl = 'https://parseapi.back4app.com';
+   await Parse().initialize(keyApplicationId, keyParseServerUrl,
+       clientKey: keyClientKey, debug: true);
+   runApp(MyApp());
+ }
+ class MyApp extends StatelessWidget {
+   Future<bool> hasUserLogged() async {
+     ParseUser? currentUser = await ParseUser.currentUser() as ParseUser?;
+     if (currentUser == null) {
+       return false;
+     }
+     //Checks whether the user's session token is valid
+     final ParseResponse? parseResponse =
+         await ParseUser.getCurrentUserFromServer(currentUser.sessionToken!);
+ 
+     if (parseResponse?.success == null || !parseResponse!.success) {
+       //Invalid session. Logout
+       await currentUser.logout();
+       return false;
+     } else {
+       return true;
+     }
+   }
+
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       title: 'Flutter - Sign In with Apple',
+       theme: ThemeData(
+         primarySwatch: Colors.blue,
+         visualDensity: VisualDensity.adaptivePlatformDensity,
+       ),
+       home: FutureBuilder<bool>(
+           future: hasUserLogged(),
+           builder: (context, snapshot) {
+             switch (snapshot.connectionState) {
+               case ConnectionState.none:
+               case ConnectionState.waiting:
+                 return Scaffold(
+                   body: Center(
+                     child: Container(
+                         width: 100,
+                         height: 100,
+                         child: CircularProgressIndicator()),
+                   ),
+                 );
+               default:
+                 if (snapshot.hasData && snapshot.data!) {
+                   return UserPage();
+                 } else {
+                   return HomePage();
+                 }
+             }
+           }),
+     );
+   }
+ }
+ 
+ class HomePage extends StatefulWidget {
+   @override
+   _HomePageState createState() => _HomePageState();
+ }
+ class _HomePageState extends State<HomePage> {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+         appBar: AppBar(
+           title: const Text('Flutter - Sign In with Apple'),
+         ),
+         body: Center(
+           child: SingleChildScrollView(
+             padding: const EdgeInsets.all(8),
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.stretch,
+               children: [
+                 Container(
+                   height: 200,
+                   child: Image.network(
+                       'http://blog.back4app.com/wp-content/uploads/2017/11/logo-b4a-1-768x175-1.png'),
+                 ),
+                 Center(
+                   child: const Text('Flutter on Back4App',
+                       style:
+                           TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                 ),
+                 SizedBox(
+                   height: 100,
+                 ),
+                 Container(
+                   height: 50,
+                   child: ElevatedButton(
+                     child: const Text('Sign In with Apple'),
+                     onPressed: () => doSignInApple(),
+                   ),
+                 ),
+                 SizedBox(
+                   height: 16,
+                 ),
+               ],
+             ),
+           ),
+         ));
+   }
+ 
+   void doSignInApple() async {
+     late ParseResponse parseResponse;
+     try {
+       //Set Scope
+       final credential = await SignInWithApple.getAppleIDCredential(
+         scopes: [
+           AppleIDAuthorizationScopes.email,
+           AppleIDAuthorizationScopes.fullName,
+         ],
+       );
+
+       //https://docs.parseplatform.org/parse-server/guide/#apple-authdata
+       //According to the documentation, we must send a Map with user authentication data.
+       //Make sign in with Apple
+       parseResponse = await ParseUser.loginWith('apple',
+           apple(credential.identityToken!, credential.userIdentifier!));
+ 
+       if (parseResponse.success) {
+         final ParseUser parseUser = await ParseUser.currentUser() as ParseUser;
+ 
+         //Additional Information in User
+         if (credential.email != null) {
+           parseUser.emailAddress = credential.email;
+         }
+         if (credential.givenName != null && credential.familyName != null) {
+           parseUser.set<String>(
+               'name', '${credential.givenName} ${credential.familyName}');
+         }
+         parseResponse = await parseUser.save();
+         if (parseResponse.success) {
+           Message.showSuccess(
+               context: context,
+               message: 'User was successfully with Sign In Apple!',
+               onPressed: () async {
+                 Navigator.pushAndRemoveUntil(
+                   context,
+                   MaterialPageRoute(builder: (context) => UserPage()),
+                   (Route<dynamic> route) => false,
+                 );
+               });
+         } else {
+           Message.showError(
+               context: context, message: parseResponse.error!.message);
+         }
+       } else {
+         Message.showError(
+             context: context, message: parseResponse.error!.message);
+       }
+     } on Exception catch (e) {
+       print(e.toString());
+       Message.showError(context: context, message: e.toString());
+     }
+   }
+ }
+
+ class UserPage extends StatelessWidget {
+   Future<ParseUser?> getUser() async {
+     return await ParseUser.currentUser() as ParseUser?;
+   }
+
+   @override
+   Widget build(BuildContext context) {
+     void doUserLogout() async {
+       final currentUser = await ParseUser.currentUser() as ParseUser;
+       var response = await currentUser.logout();
+       if (response.success) {
+         Message.showSuccess(
+             context: context,
+             message: 'User was successfully logout!',
+             onPressed: () {
+               Navigator.pushAndRemoveUntil(
+                 context,
+                 MaterialPageRoute(builder: (context) => HomePage()),
+                 (Route<dynamic> route) => false,
+               );
+             });
+       } else {
+         Message.showError(context: context, message: response.error!.message);
+       }
+     }
+ 
+     return Scaffold(
+         appBar: AppBar(
+           title: Text('Flutter - Sign In with Apple'),
+         ),
+         body: FutureBuilder<ParseUser?>(
+             future: getUser(),
+             builder: (context, snapshot) {
+               switch (snapshot.connectionState) {
+                 case ConnectionState.none:
+                 case ConnectionState.waiting:
+                   return Center(
+                     child: Container(
+                         width: 100,
+                         height: 100,
+                         child: CircularProgressIndicator()),
+                   );
+                 default:
+                   return Padding(
+                     padding: const EdgeInsets.all(8.0),
+                     child: Column(
+                       crossAxisAlignment: CrossAxisAlignment.stretch,
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       children: [
+                         Center(
+                             child: Text(
+                                 'Hello, ${snapshot.data!.get<String>('name')}')),
+                         SizedBox(
+                           height: 16,
+                         ),
+                         Container(
+                           height: 50,
+                           child: ElevatedButton(
+                             child: const Text('Logout'),
+                             onPressed: () => doUserLogout(),
+                           ),
+                         ),
+                       ],
+                     ),
+                   );
+               }
+             }));
+   }
+ }
+ 
+ class Message {
+   static void showSuccess(
+       {required BuildContext context,
+       required String message,
+       VoidCallback? onPressed}) {
+     showDialog(
+       context: context,
+       builder: (BuildContext context) {
+         return AlertDialog(
+           title: const Text("Success!"),
+           content: Text(message),
+           actions: <Widget>[
+             new ElevatedButton(
+               child: const Text("OK"),
+               onPressed: () {
+                 Navigator.of(context).pop();
+                 if (onPressed != null) {
+                   onPressed();
+                 }
+               },
+             ),
+           ],
+         );
+       },
+     );
+   }
+ 
+   static void showError(
+       {required BuildContext context,
+       required String message,
+       VoidCallback? onPressed}) {
+     showDialog(
+       context: context,
+       builder: (BuildContext context) {
+         return AlertDialog(
+           title: const Text("Error!"),
+           content: Text(message),
+           actions: <Widget>[
+             new ElevatedButton(
+               child: const Text("OK"),
+               onPressed: () {
+                 Navigator.of(context).pop();
+                 if (onPressed != null) {
+                   onPressed();
+                 }
+               },
+             ),
+           ],
+         );
+       },
+     );
+   }
+ }
